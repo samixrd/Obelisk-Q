011b0960289e66710c368ac9b4666034b2e20327
samixrd <samixrd3@gmail.com>
2026-06-03 23:32:59 -0700

    chore: upgrade solidity compiler version to 0.8.32 to resolve compiler warnings

--- contracts/contracts/ObeliskVault.sol
+++ contracts/contracts/ObeliskVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.32;
 
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
--- contracts/contracts/ZKRegimeVerifier.sol
+++ contracts/contracts/ZKRegimeVerifier.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.32;
 
 interface IZKRegimeVerifier {
     function verifyRegimeProof(
